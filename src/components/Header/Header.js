589ed64258ad298bb37cdcdadb93985c677a22d6
Mohamad23-05 <67656497+Mohamad23-05@users.noreply.github.com>
2023-05-21 14:04:48 +0200

    Header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,34 @@
 import './Header.css';
 
+import logo from '../../assets/images/logo.png'
+import NavItem , {NavItemDropDown} from '../navItem/navItem'
 const Header = () => {
   return (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="google.de">Moka</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="google.de">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="google.de">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="google.de" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="googe.de">Action</a></li>
-            <li><a className="dropdown-item" href="google.de">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="google.de">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="google.de" tabIndex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+<div className="navbar navbar-expand-md bg-dark navbar-dark">
+        <div className="container">
+            <a href="#" className="navbar-brand">
+              <img src={logo} alt="" />
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="mainmenu">
+                <ul className="navbar-nav ms-auto">
+                    <NavItem><a href="#hero" className="nav-link">Home</a></NavItem>
+                    <NavItem><a href="#features" className="nav-link">Browse</a></NavItem>
+
+                    <NavItemDropDown>
+                        <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Details</a>
+                        <ul className="dropdown-menu">
+                            <li><a href="#learn" className="dropdown-item">Learn Bootstrap</a></li>
+                            <li><a href="#next" className="dropdown-item">Where to go next</a></li>
+                        </ul>
+                    </NavItemDropDown>
+                    <NavItem><a href="#faq" className="nav-link">Ask Me</a></NavItem>
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
-</nav>
   )
 }
 
